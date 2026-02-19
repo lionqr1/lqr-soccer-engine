9368d8095d525cdec0e295fff6b9c254228c6f04
lionqr1 <lionqr11@outlook.com>
2026-02-19 13:44:58 -0500

    Implement World class with teams and ball management

--- src/engine/World.js
+++ src/engine/World.js
@@ -1,1 +1,20 @@
+export class World {
+  constructor(width = 105, height = 68) {
+    this.width = width;
+    this.height = height;
+    this.teams = [];
+    this.ball = null;
+  }
 
+  addTeam(team) {
+    this.teams.push(team);
+  }
+
+  setBall(ball) {
+    this.ball = ball;
+  }
+
+  getAllPlayers() {
+    return this.teams.flatMap(t => t.players);
+  }
+}
