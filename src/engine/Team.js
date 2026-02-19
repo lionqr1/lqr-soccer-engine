698beb8cc707db158e7b2255236cd1fecd9b95ca
lionqr1 <lionqr11@outlook.com>
2026-02-19 13:44:43 -0500

    Add Team class with player management

--- src/engine/Team.js
+++ src/engine/Team.js
@@ -1,1 +1,11 @@
+export class Team {
+  constructor(id, players = []) {
+    this.id = id;
+    this.players = players;
+    this.score = 0;
+  }
 
+  addPlayer(player) {
+    this.players.push(player);
+  }
+}
