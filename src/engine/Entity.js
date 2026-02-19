9cef9ff7b701fa8ffabeacad46748b0fcc8ee721
lionqr1 <lionqr11@outlook.com>
2026-02-19 13:42:41 -0500

    Add Entity class with position and velocity properties

--- src/engine/Entity.js
+++ src/engine/Entity.js
@@ -1,1 +1,14 @@
+export class Entity {
+  constructor(id, x = 0, y = 0) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+  }
 
+  update(dt) {
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
+  }
+}
