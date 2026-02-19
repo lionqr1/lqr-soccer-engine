68fb959029bb98450011b96569a002e057fc8dc3
lionqr1 <lionqr11@outlook.com>
2026-02-19 13:43:36 -0500

    Add Ball class extending Entity with properties

--- src/engine/Ball.js
+++ src/engine/Ball.js
@@ -1,1 +1,9 @@
+import { Entity } from './Entity.js';
 
+export class Ball extends Entity {
+  constructor(id = "ball", x = 0, y = 0) {
+    super(id, x, y);
+    this.radius = 0.11;
+    this.airDrag = 0.02;
+  }
+}
